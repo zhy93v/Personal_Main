b93a1dcd66ca1c69bbae097a121f90df2ed29ba1
zhy93vv <155813779+zhy93vv@users.noreply.github.com>
2024-03-13 00:01:00 +0900

    update

--- C_Basic/Hello.c
+++ C_Basic/Hello.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+
+
 int main()
 {
+	char ch; // 문자
+
+	short sh; // 정수
+	int i;
+	long lo;
+
+	float fl; // 실수
+	double du;
+
+
+	printf("char 형 변수 입력 : ");
+	scanf("%c", &ch);
+
+	printf("short 형 변수 입력 : ");
+	scanf("%hd", &sh);
+
+	printf("char : %c , short : %d", ch, sh);
 
 
 	return 0;
